bff877ec82ce5053b81b59256f16970c7394f9df
Shekar Ramaswamy <shekarramaswamy@neils-macbook-air.local>
2017-01-22 13:29:19 -0800

    Initial Commit

---
+++ MinesweeperUI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MinesweeperUI
+//
+//  Created by Shekar Ramaswamy on 1/22/17.
+//  Copyright © 2017 Shekar Ramaswamy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
