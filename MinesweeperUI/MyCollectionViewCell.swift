6b32daa35564fc993133a25d99b29636a93fa86e
Shekar Ramaswamy <shekarramaswamy@neils-macbook-air.local>
2017-01-22 16:21:26 -0800

    Basic UI working!

---
+++ MinesweeperUI/MyCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MyCollectionViewCell.swift
+//  MinesweeperUI
+//
+//  Created by Shekar Ramaswamy on 1/22/17.
+//  Copyright © 2017 Shekar Ramaswamy. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var myLabel: UILabel!
+    
+}
--- MinesweeperUI/ViewController.swift
+++ MinesweeperUI/ViewController.swift
@@ -5,23 +5,48 @@
 //  Created by Shekar Ramaswamy on 1/22/17.
 //  Copyright © 2017 Shekar Ramaswamy. All rights reserved.
 //
-
+    
 import UIKit
-
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let reuseIdentifier = "cell" // also enter this string as the cell identifier in the storyboard
+    var items = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48"]
+    
+    
+    // MARK: - UICollectionViewDataSource protocol
+    
+    // tell the collection view how many cells to make
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.items.count
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // make a cell for each cell index path
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        // get a reference to our storyboard cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! MyCollectionViewCell
+        
+        // Use the outlet in our custom class to get a reference to the UILabel in the cell
+        cell.myLabel.text = self.items[indexPath.item]
+        cell.backgroundColor = UIColor.cyan // make cell more visible in our example project
+        
+        return cell
     }
-
-
+    
+    // MARK: - UICollectionViewDelegate protocol
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // handle tap events
+        print("You selected cell #\(indexPath.item)!")
+    }
+    
+    // override func viewDidLoad() {
+        // super.viewDidLoad()
+        // self.collectionView.register(MyCollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+    //}
 }
+
 
 
 /**
@@ -33,7 +58,7 @@ class ViewController: UIViewController {
  handle click by updating board state then repainting board state
  handle bomb or victory
  
- reset button - 
+ reset button -
  two screen ui with home and game/feature
  make icons
  */
