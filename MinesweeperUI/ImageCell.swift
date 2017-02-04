8980240f8ed049a1df15c56daccb79cd342a8983
Shekar Ramaswamy <shekarramaswamy@neils-macbook-air.local>
2017-02-04 15:43:24 -0800

    more work on image uploader

---
+++ MinesweeperUI/ImageCell.swift
@@ -0,0 +1,15 @@
+//
+//  ImageCell.swift
+//  MinesweeperUI
+//
+//  Created by Shekar Ramaswamy on 2/4/17.
+//  Copyright © 2017 Shekar Ramaswamy. All rights reserved.
+//
+
+import UIKit
+
+class ImageCell:UICollectionViewCell
+{
+    @IBOutlet weak var image: UIImageView!
+    
+}
--- MinesweeperUI/ImageUploaderController.swift
+++ MinesweeperUI/ImageUploaderController.swift
@@ -10,5 +10,18 @@ import UIKit
 
 class ImageUploaderController: UICollectionView
 {
-     
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let reuseIdentifier = "cell"
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! ImageCell
+        
+        return cell
+    }
+    
 }
